b20f01eca2b52323f09bfce3151a181253927580
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-17 11:57:56 +0200

    Temp Mole prefab
    amazing, i know

--- HoleMole/Assets/Scripts/Hole.cs
+++ HoleMole/Assets/Scripts/Hole.cs
@@ -7,20 +7,20 @@ public class Hole
     private HoleStatus status = HoleStatus.None;
     private int x;
     private int z;
-    private Field world;
+    private Field field;
 
     public HoleStatus Status { get => status; set => status = value; }
 
-    public Hole(int x, int z, Field world)
+    public Hole(int x, int z, Field field)
     {
         this.x = x;
         this.z = z;
-        this.world = world;
+        this.field = field;
 
         // Set starting area 3x3 in the middle
-        if (this.x >= (world.Width - 2) /2 && this.x <= (world.Width + 2) / 2)
+        if (this.x >= (field.Width - 2) /2 && this.x <= (field.Width + 2) / 2)
         {
-            if (this.z >= (world.Depth - 2) / 2 && this.z <= (world.Depth + 2) / 2)
+            if (this.z >= (field.Depth - 2) / 2 && this.z <= (field.Depth + 2) / 2)
             {
                 status = HoleStatus.Empty;
             }
