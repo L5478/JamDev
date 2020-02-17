e4ecf4bb7fcf21abdddd4c8e818de9912126f051
cedricvoncken <cedric.voncken@student.hu.nl>
2020-02-17 11:08:59 +0100

    GridTest | Started with some grid creations

---
+++ HoleMole/Assets/Scripts/GridManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject hole;
+
+    void Start()
+    {
+        Grid grid = new Grid(10, 10, 2f, hole);
+    }
+}
