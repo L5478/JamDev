5d1ac54d8f9b5b32cc5c0991ad179cfff4a6389c
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-17 13:12:30 +0200

    Project added

---
+++ HoleMole/Assets/Scripts/Mole.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+
+
+    private Hole hole;
+    private FieldController fieldController;
+
+    private void Start()
+    {
+        fieldController = FindObjectOfType<FieldController>();
+
+        hole = fieldController.Field.GetEmptyHole();
+        hole.Status = Hole.HoleStatus.Mole;
+
+        transform.position = hole.Position;
+    }
+}
