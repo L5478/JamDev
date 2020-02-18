6e91c4a8d4f199d875d617af551d8a85a646a45f
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-18 18:26:54 +0200

    New project created with gitignore

---
+++ HoleMoleNew/Assets/Scripts/FieldController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    #region Singleton
+    public static FieldController instance;
+    public static FieldController Instance { get => instance; }
+    #endregion
+
+    public GameObject holePrefab;
+
+    public float emptySpace = 1f;
+    public int holesX;
+    public int holesZ;
+
+    private Vector3 offset;
+
+    public Field Field;
+    private List<Transform> holeGFX = new List<Transform>();
+
+    private Dictionary<Hole, GameObject> holeGODictonary = new Dictionary<Hole, GameObject>();
+
+    private void Awake()
+    {
+        #region Singleton
+        if (instance != null && instance != this)
+            Destroy(instance);
+        else
+            instance = this;
+        #endregion
+
+        // Prevent wierd behaviour
+        if (emptySpace <= 0)
+            emptySpace = 1;
+
+        // Offset, so Holes will be build around of this gameobject
+        // i.e. Hole in center of field will sit on the same position as this gameobject
+        offset = new Vector3(-(holesX - 1) / 2, 0, -(holesZ - 1) / 2);
+
+        // Create starting field / world
+        Field = new Field(holesX, holesZ);
+
+        // Loop through field
+        for (int x = 0; x < Field.Width; x++)
+        {
+            for (int z = 0; z < Field.Depth; z++)
+            {
+                // Instantiate Hole gameobject, rename it and get all children obejcts (graphics)
+                GameObject holeGO = Instantiate(holePrefab, (new Vector3(x, 0, z) + offset) * emptySpace, Quaternion.identity, this.transform);
+                holeGO.name = "Hole_" + x + "_" + z;
+
+                // Get right Hole information
+                Hole hole_data = Field.GetHoleAt(x, z);
+                hole_data.Position = holeGO.transform.position;
+
+                holeGODictonary.Add(hole_data, holeGO);
+
+                // Set right graphics based on Hole status
+                SwitchHoleGFX(hole_data);
+            }
+        }
+    }
+
+    public void SwitchHoleGFX(Hole hole)
+    {
+        switch (hole.Status)
+        {
+            case Hole.HoleStatus.None:
+                SetGFXByTag("HoleNone", hole);
+                break;
+            case Hole.HoleStatus.Empty:
+                SetGFXByTag("HoleEmpty", hole);
+                break;
+            case Hole.HoleStatus.Mole:
+                SetGFXByTag("HoleMole", hole);
+                break;
+            case Hole.HoleStatus.Plank:
+                SetGFXByTag("HolePlank", hole);
+                break;
+            case Hole.HoleStatus.Water:
+                SetGFXByTag("HoleWater", hole);
+                break;
+            case Hole.HoleStatus.Fire:
+                SetGFXByTag("HoleFire", hole);
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Set right grahics to Hole gameobject by tag
+    private void SetGFXByTag(string tag, Hole hole)
+    {
+        GameObject holeGO = holeGODictonary[hole];
+
+        foreach (var GFX in holeGO.GetComponentsInChildren<Transform>(true))
+        {
+            if (GFX.gameObject.CompareTag("Hole"))
+            { 
+                continue;
+            }
+
+            if (GFX.gameObject.CompareTag(tag) == true)
+            {
+                GFX.gameObject.SetActive(true);
+            }
+            else
+            { 
+                GFX.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ HoleMoleNew/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Texture2D PlankImg;
+    public Texture2D WaterImg;
+    public Texture2D FireImg;
+
+    public enum PowerUp { None, Plank, Water, Fire }
+
+    private PowerUp currentPowerUp = PowerUp.None;
+
+    public static event Action<Transform> moleHitted;
+    public static event Action<Transform> waterPowerUP;
+
+    void Update()
+    {
+        //Mouse button down if not hovering over any UI elements
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (currentPowerUp == PowerUp.None)
+                {
+                    //Regular hit mole
+                    moleHitted?.Invoke(hit.transform);
+                }
+                else
+                {
+                    //Powerup hit hole
+                    Debug.Log("used: " + currentPowerUp.ToString() + " powerup on: " + hit.transform.parent);
+                    currentPowerUp = PowerUp.None;
+                    SetCursorImage();
+                    waterPowerUP?.Invoke(hit.transform);
+                }
+            }
+        }
+    }
+
+    public void SetCurrentPowerUp(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                currentPowerUp = PowerUp.Plank;
+                SetCursorImage(PlankImg);
+                break;
+            case 2:
+                currentPowerUp = PowerUp.Water;
+                SetCursorImage(WaterImg);
+                break;
+            case 3:
+                currentPowerUp = PowerUp.Fire;
+                SetCursorImage(FireImg);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SetCursorImage(Texture2D img = null)
+    {
+        //if method called empty -> reset image
+        if (img == null)
+        {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
+        else
+        {
+            Cursor.SetCursor(img, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
