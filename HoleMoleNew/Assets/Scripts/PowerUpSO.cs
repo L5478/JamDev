aa863ee1415b0136c682cc26e276538de1b3097f
cedricvoncken <cedric.voncken@student.hu.nl>
2020-02-19 22:36:45 +0100

    meh

---
+++ HoleMoleNew/Assets/Scripts/PowerUpSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "PowerUpSO", menuName = "ScriptableObjects/PowerUpSO")]
+public class PowerUpSO : ScriptableObject
+{
+    public new string name;
+    public Sprite spriteImg;
+    public Texture2D cursorImg;
+    public int weight;
+}
--- HoleMoleNew/Assets/Scripts/PowerUpSelector.cs
+++ HoleMoleNew/Assets/Scripts/PowerUpSelector.cs
@@ -7,6 +7,10 @@ public class PowerUpSelector : MonoBehaviour
 {
     public GameObject powerupsGO;
     public GameObject grayBackground;
+    public List<PowerUpSO> listPowerUps;
+    public Button Spot1;
+    public Button Spot2;
+    
 
     private PlayerInput playerInput;
     private Animator animator;
@@ -22,7 +26,16 @@ public class PowerUpSelector : MonoBehaviour
     private void StartPowerupsAnimation ()
     {
         animator.SetTrigger("ShowPowerUps");
-        Time.timeScale = .25f; 
+        Time.timeScale = .20f;
+
+        var rdm1 = GetRandomPowerUp();
+        var rdm2 = GetRandomPowerUp();
+        Spot1.GetComponentInChildren<Text>().text = rdm1.name;
+        Spot1.GetComponent<Image>().sprite = rdm1.spriteImg;
+        Spot2.GetComponentInChildren<Text>().text = rdm2.name;
+        Spot2.GetComponent<Image>().sprite = rdm2.spriteImg;
+
+
     }
 
     public void OnPowerUpSelect(int iPowerUp)
@@ -31,5 +44,28 @@ public class PowerUpSelector : MonoBehaviour
         playerInput.SetCurrentPowerUp(iPowerUp);
         animator.SetTrigger("Hide");
         Invoke("StartPowerupsAnimation", waitTime * 2f);
+    }
+
+    public PowerUpSO GetRandomPowerUp()
+    {
+        int totalWeight = 0;
+        //add up total weight values
+        foreach (var PU in listPowerUps)
+        {
+            totalWeight += PU.weight;
+        }
+
+        //Get random int in range
+        float randomN = Random.Range(0, totalWeight);
+
+        //Keep subtracting untill you find your random weighed number
+        for (int i = 0; i < listPowerUps.Count; i++)
+        {
+            if (randomN < listPowerUps[i].weight) {
+                return listPowerUps[i];
+            }
+            randomN -= listPowerUps[i].weight;
+        }
+        return null;
     }
 }
