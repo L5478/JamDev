f9418f39ea38ae7cda4464f0015057fd66bf7367
cedricvoncken <cedric.voncken@student.hu.nl>
2020-02-20 21:46:57 +0100

    Mallet main menu

--- HoleMoleNew/Assets/Scripts/Menu/MenuInput.cs
+++ HoleMoleNew/Assets/Scripts/Menu/MenuInput.cs
@@ -2,8 +2,13 @@
 
 public class MenuInput : MonoBehaviour
 {
+    public GameObject Hammer;
+    private Animator HammerAnim;
 
-
+    private void Start()
+    {
+        HammerAnim = Hammer.GetComponent<Animator>();
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -14,6 +19,7 @@ public class MenuInput : MonoBehaviour
                 MenuMole mole = hit.transform.GetComponentInParent<MenuMole>();
                 if (mole != null)
                 {
+                    HammerAnim.SetTrigger("SLAP");
                     StartCoroutine(mole.GetHit());
                 }
             }
