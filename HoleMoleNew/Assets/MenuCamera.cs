06995b56110f8a0ba0eda544536f2d96c3fbc185
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-20 16:35:45 +0200

    Menu scene

---
+++ HoleMoleNew/Assets/MenuCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour
+{
+    public Transform[] pointList;
+    public Transform lookAt;
+    public float speed;
+
+    private Vector3 endpos;
+    private Vector3 startpos;
+    private Vector3 nextpos;
+
+    private float time = 0;
+
+    private void Start()
+    {
+        endpos = pointList[0].position;
+        startpos = pointList[1].position;
+        transform.position = startpos;
+    }
+
+    void Update()
+    {
+        transform.LookAt(lookAt);
+
+        time += Time.deltaTime;
+
+        transform.position = Vector3.Lerp(startpos, endpos, time * speed);
+        float x = transform.position.x;
+
+        if (x == endpos.x)
+        {
+            endpos = endpos == pointList[0].position ? pointList[1].position : pointList[0].position;
+            startpos = startpos == pointList[0].position ? pointList[1].position : pointList[0].position;
+
+            time = 0;
+        }
+    }
+}
