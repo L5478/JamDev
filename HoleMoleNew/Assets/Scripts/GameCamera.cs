5907cb70ea6c45eba54e2b2c800f8ac5fb25a08c
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-21 11:27:31 +0200

    Camera that doesn't work properly

--- HoleMoleNew/Assets/Scripts/GameCamera.cs
+++ HoleMoleNew/Assets/Scripts/GameCamera.cs
@@ -6,26 +6,37 @@ public class GameCamera : MonoBehaviour
     public Transform nearestPos;
     public Transform farestPos;
 
-    
+    public Transform farestTow;
 
+    public float scrollSpeed = .1f;
+    public float maxScrollSpeed = 1;
+    public float minScrollSpeed = -1;
+    public float zoomSpeed = .1f;
+
+    private float time = 0;
+
+    // THIS IS NOT WORKINIINGSDAAFDSAFe
     private void Update()
     {
-        float scrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+        time += Time.deltaTime;
 
+        float scrollWheelChange = -Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
 
-        if (scrollWheelChange > 0)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, farestPos.position, scrollWheelChange);
-        }
-        else if (scrollWheelChange < 0)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, nearestPos.position, scrollWheelChange);
-        }
+        scrollWheelChange = Mathf.Clamp(scrollWheelChange, minScrollSpeed, maxScrollSpeed);
 
         transform.LookAt(lookAt);
 
+        Vector3 targetPos = Vector3.MoveTowards(transform.position, farestTow.position, scrollWheelChange);
 
+        float farDistance = Vector3.Distance(targetPos, farestPos.position);
+        float nearDistance = Vector3.Distance(targetPos, nearestPos.position);
 
+        if (farDistance <= 5 || nearDistance <= 5)
+        {
+            return;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, time * zoomSpeed);
     }
 
 
