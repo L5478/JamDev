5cbbbbf8b9825f46d7c91a2995ec4c38d45ff5d3
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-21 10:52:44 +0200

    Field area suggestion + camera movement started

---
+++ HoleMoleNew/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public Transform lookAt;
+    public Transform nearestPos;
+    public Transform farestPos;
+
+    
+
+    private void Update()
+    {
+        float scrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+
+
+        if (scrollWheelChange > 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, farestPos.position, scrollWheelChange);
+        }
+        else if (scrollWheelChange < 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nearestPos.position, scrollWheelChange);
+        }
+
+        transform.LookAt(lookAt);
+
+
+
+    }
+
+
+}
