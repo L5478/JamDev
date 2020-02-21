67a3b38adea962f4adefe8527639d311e53e7beb
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-21 16:33:59 +0200

    Sounds moles

--- HoleMoleNew/Assets/Scripts/Menu/MenuMole.cs
+++ HoleMoleNew/Assets/Scripts/Menu/MenuMole.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class MenuMole : MonoBehaviour
 {
+    [Header("Don't touch")]
     public float health;
-
     public GameObject normalMole;
     public GameObject eliteMole;
-
     public GameObject normalDamageEffect;
     public GameObject eliteDamageEffect;
     public GameObject helmetEffect;
     public GameObject helmet;
 
     private GameObject currentMole;
+    private AudioClip currentDeadSound;
     private GameObject currentDamageEffect;
 
     private void Start()
@@ -30,11 +30,13 @@ public class MenuMole : MonoBehaviour
     public IEnumerator GetHit()
     {
         health--;
+        
 
         if (currentMole == eliteMole && helmet.activeInHierarchy)
         {
             helmetEffect.SetActive(true);
             helmet.SetActive(false);
+            eliteMole.GetComponentInChildren<MoleSoundEffects>().PlayHelmethitSound();
         }
 
         if (health <= 0)
@@ -43,26 +45,25 @@ public class MenuMole : MonoBehaviour
             currentMole.GetComponentInChildren<Animator>().SetTrigger("Hit");
         } 
 
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.7f);
 
         if (health <= 0)
-        {
             currentMole.gameObject.SetActive(false);
-        }
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3f);
 
         if (health <= 0)
         {
             currentMole = currentMole == normalMole ? eliteMole : normalMole;
+
             health = currentMole == normalMole ? 1f : 2f;
             currentDamageEffect = currentMole == normalMole ? normalDamageEffect : eliteDamageEffect;
+
             currentDamageEffect.SetActive(false);
             currentMole.SetActive(true);
+
             if (helmet.activeInHierarchy == false)
-            {
                 helmet.SetActive(true);
-            }
         }
     }
 }
---
+++ HoleMoleNew/Assets/Scripts/Menu/MoleSoundEffects.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MoleSoundEffects : MonoBehaviour
+{
+    [Header("Shared Sound effects")]
+    public AudioClip[] hitSounds;
+    public AudioClip[] waterDeadSounds;
+    public AudioClip[] normalDigSounds;
+
+    [Header("Normal mole Sound effects")]
+    public AudioClip[] normalDeadSounds;
+    public AudioClip[] normalAppearSounds;
+    public AudioClip[] plankBlockSounds;
+
+    [Header("Elite mole Sound effects")]
+    public AudioClip[] helmetHitSounds;
+    public AudioClip[] eliteDeadSounds;
+    public AudioClip[] eliteDigSounds;
+    public AudioClip[] eliteAppearSounds;
+    public AudioClip[] plankBreakSounds;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public AudioClip GetRandomSound(AudioClip[] audioClips)
+    {
+        return audioClips[Random.Range(0, audioClips.Length)];
+    }
+
+    public void PlayDeadSound(string moleType)
+    {
+        switch (moleType)
+        {
+            case "Normal":
+                audioSource.PlayOneShot(GetRandomSound(normalDeadSounds));
+                break;
+            case "Elite":
+                audioSource.PlayOneShot(GetRandomSound(eliteDeadSounds));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void PlayAppearSound(string moleType)
+    {
+        switch (moleType)
+        {
+            case "Normal":
+                audioSource.PlayOneShot(GetRandomSound(normalAppearSounds));
+                break;
+            case "Elite":
+                audioSource.PlayOneShot(GetRandomSound(eliteAppearSounds));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void PlayDigSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(normalDigSounds));
+    }
+
+    public void PlaySuperDigSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(eliteDigSounds));
+    }
+
+    public void PlayNormalhitSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(hitSounds));
+    }
+
+    public void PlayWaterDeadSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(waterDeadSounds));
+    }
+
+    public void PlayHelmethitSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(helmetHitSounds));
+    }
+
+    public void PlayPlankBreakSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(plankBreakSounds));
+    }
+
+    public void PlayPlankBlockSound()
+    {
+        audioSource.PlayOneShot(GetRandomSound(plankBlockSounds));
+    }
+
+}
