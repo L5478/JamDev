3f100a05be11500d4553de90eec4a89ba9a9961c
Pekka Kääriäinen <43665761+L5478@users.noreply.github.com>
2020-02-21 16:46:13 +0200

    M like MUTE

---
+++ HoleMoleNew/Assets/Scripts/Options.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+
+    private AudioListener audioListener;
+
+    private void Start()
+    {
+        audioListener = FindObjectOfType<AudioListener>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleMute();
+        }
+    }
+
+    private void ToggleMute()
+    {
+        AudioListener.volume = AudioListener.volume == 1 ? 0 : 1;
+    }
+}
